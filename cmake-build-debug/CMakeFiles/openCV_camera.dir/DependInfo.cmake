76598bb8c648f16455653c20fbfb59710f99b12a
zhaoxuyan <526161421@qq.com>
2018-04-29 16:24:49 +0800

    add cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/openCV_camera.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/zhaoxuyan/CLionProjects/openCV_camera/openCV_Canny.cpp" "/Users/zhaoxuyan/CLionProjects/openCV_camera/cmake-build-debug/CMakeFiles/openCV_camera.dir/openCV_Canny.cpp.o"
+  "/Users/zhaoxuyan/CLionProjects/openCV_camera/openCV_ChineseWrite.cpp" "/Users/zhaoxuyan/CLionProjects/openCV_camera/cmake-build-debug/CMakeFiles/openCV_camera.dir/openCV_ChineseWrite.cpp.o"
+  "/Users/zhaoxuyan/CLionProjects/openCV_camera/openCV_FaceDetection.cpp" "/Users/zhaoxuyan/CLionProjects/openCV_camera/cmake-build-debug/CMakeFiles/openCV_camera.dir/openCV_FaceDetection.cpp.o"
+  "/Users/zhaoxuyan/CLionProjects/openCV_camera/openCV_FaceDetection_Blur.cpp" "/Users/zhaoxuyan/CLionProjects/openCV_camera/cmake-build-debug/CMakeFiles/openCV_camera.dir/openCV_FaceDetection_Blur.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/zhaoxuyan/anaconda/include/opencv"
+  "/Users/zhaoxuyan/anaconda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/openCV_camera.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openCV_camera.dir/openCV_ChineseWrite.cpp.o"
+  "CMakeFiles/openCV_camera.dir/openCV_Canny.cpp.o"
+  "CMakeFiles/openCV_camera.dir/openCV_FaceDetection.cpp.o"
+  "CMakeFiles/openCV_camera.dir/openCV_FaceDetection_Blur.cpp.o"
+  "openCV_camera.pdb"
+  "openCV_camera"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openCV_camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
